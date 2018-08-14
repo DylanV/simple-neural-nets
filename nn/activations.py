42efe09cad030b6fbd9cbb46502562bc8adf74d1
DylanV <dverrezen@gmail.com>
2018-08-14 15:48:38 +0200

    Add docstrings and test for sigmoid derivative

--- nn/activations.py
+++ nn/activations.py
@@ -1,9 +1,38 @@
-"""Nonlinear activation functions"""
+"""Nonlinear activation functions."""
+
+from typing import Union
 
 import numpy as np
 
-def sigmoid(x):
+
+def sigmoid(x: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
+    """Compute the logistic function for a given input x.
+
+    Parameters
+    ----------
+    x : ndarray of float
+
+    Returns
+    -------
+    activation : ndarray
+       the sigmoid of x
+
+    """
     return 1 / (1 + np.exp(-x))
 
-def sigmoid_derivative(x):
+
+def sigmoid_derivative(x: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
+    """Compute the derivative of the sigmoid function for the given input x.
+
+    Parameters
+    ----------
+    x : ndarray of float
+        Where the derivative will be evaluated
+
+    Returns
+    -------
+    derivative : ndarray of float
+        The derivative of the sigmoid function at x
+
+    """
     return sigmoid(x) * (1 - sigmoid(x))
--- nn/tests/test_activations.py
+++ nn/tests/test_activations.py
@@ -1,8 +1,10 @@
-"""Tests for nonlinear activation functions"""
+"""Tests for nonlinear activation functions."""
 
+import numpy as np
 import numpy.testing as npt
 
-from nn.activations import *
+from nn.activations import sigmoid, sigmoid_derivative
+from nn.utils import finite_difference_derivative
 
 
 def test_sigmoid_float():
@@ -23,5 +25,13 @@ def test_sigmoid_ndarray():
     npt.assert_allclose(sigmoid(np.array([-6, 6])), np.array([0, 1]), atol=1e-2)
     npt.assert_allclose(sigmoid(np.array([-10, 10])), np.array([0, 1]), atol=1e-4)
     # Check preserves shape
-    assert sigmoid(np.zeros((2,1))).shape == (2,1)
-    assert sigmoid(np.zeros((2,))).shape == (2,)
+    assert sigmoid(np.zeros((2, 1))).shape == (2, 1)
+    assert sigmoid(np.zeros((2, ))).shape == (2, )
+
+
+def test_sigmoid_derivative():
+    npt.assert_almost_equal(sigmoid_derivative(0), finite_difference_derivative(sigmoid, 0))
+    npt.assert_almost_equal(sigmoid_derivative(3), finite_difference_derivative(sigmoid, 3))
+    npt.assert_almost_equal(sigmoid_derivative(-3), finite_difference_derivative(sigmoid, -3))
+
+
