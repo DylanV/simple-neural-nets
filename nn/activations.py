1e6cdcc31ec82d34295d463101e1522f84a9e9c1
DylanV <dverrezen@gmail.com>
2018-08-22 21:51:19 +0200

    Convert activations for autograd

--- nn/activations.py
+++ nn/activations.py
@@ -1,7 +1,10 @@
 """Nonlinear activation functions."""
 
-import numpy as np
 from abc import ABC, abstractmethod
+
+import numpy as np
+
+from nn.weights import initialise_weights
 
 
 class Activation(ABC):
@@ -15,8 +18,34 @@ class Activation(ABC):
         pass
 
 
+class Linear(Activation):
+    """Linear layer"""
+
+    def __init__(self, in_size: int, out_size: int, initialisation_method: str='xavier-average'):
+        self.weights = initialise_weights((in_size, out_size), method=initialisation_method)
+        self.biases = np.zeros((1, out_size))
+
+        self._cached_activations = None
+        self._weight_gradients = np.zeros(self.weights.shape)
+        self._bias_gradients = np.zeros(self.biases.shape)
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        activations = np.dot(x, self.weights) + self.biases
+        self._cached_activations = activations
+        return activations
+
+    def backward(self, error: np.ndarray) -> np.ndarray:
+        # Update the gradients
+        self._bias_gradients = np.sum(error, 0)
+        self._weight_gradients = np.dot(self._cached_activations.T, error)
+        return np.dot(error, self.weights.T)
+
+
 class Sigmoid(Activation):
     """Logistic sigmoid activation"""
+
+    def __init__(self):
+        self.cached_input = None
 
     @staticmethod
     def _sigmoid(x: np.ndarray) -> np.ndarray:
@@ -35,14 +64,15 @@ class Sigmoid(Activation):
            the sigmoid of x
 
         """
+        self.cached_input = x
         return self._sigmoid(x)
 
-    def backward(self, x: np.ndarray) -> np.ndarray:
+    def backward(self, error: np.ndarray) -> np.ndarray:
         """Compute the derivative of the sigmoid function for the given input x.
 
         Parameters
         ----------
-        x : ndarray of float
+        error : ndarray of float
             Where the derivative will be evaluated
 
         Returns
@@ -51,11 +81,15 @@ class Sigmoid(Activation):
             The derivative of the sigmoid function at x
 
         """
-        return self._sigmoid(x) * (1 - self._sigmoid(x))
+        derivative = self._sigmoid(self.cached_input) * (1 - self._sigmoid(self.cached_input))
+        return error * derivative
 
 
 class Tanh(Activation):
     """Tanh activation"""
+
+    def __init__(self):
+        self._cached_input = None
 
     @staticmethod
     def _tanh(x: np.ndarray) -> np.ndarray:
@@ -73,25 +107,29 @@ class Tanh(Activation):
         activation : ndarray of float
 
         """
+        self._cached_input = x
         return self._tanh(x)
 
-    def backward(self, x: np.ndarray) -> np.ndarray:
+    def backward(self, error: np.ndarray) -> np.ndarray:
         """Compute the derivative of the tanh function at x.
 
         Parameters
         ----------
-        x : ndarray of float
+        error : ndarray of float
 
         Returns
         -------
         derivative : ndarray of float
 
         """
-        return 1 - self._tanh(x) ** 2
+        return error * (1 - self._tanh(self._cached_input) ** 2)
 
 
 class ReLU(Activation):
     """Rectified Linear Unit activation"""
+
+    def __init__(self):
+        self.cached_input = None
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         """Rectified Linear Unit.
@@ -108,16 +146,16 @@ class ReLU(Activation):
         """
         return np.maximum(0, x)
 
-    def backward(self, x: np.ndarray) -> np.ndarray:
+    def backward(self, error: np.ndarray) -> np.ndarray:
         """Compute the derivative of the ReLU function with respect to it's input at x.
 
         Parameters
         ----------
-        x : ndarray of float
+        error : ndarray of float
 
         Returns
         -------
         derivative : ndarray of float
 
         """
-        return np.asarray(x >= 0, np.float)
+        return error * np.asarray(self.cached_input >= 0, np.float)
