b9954f1ae0e055eaab7c55fd05fc325868d477da
DylanV <dverrezen@gmail.com>
2018-08-22 21:50:49 +0200

    Add sequential model skeleton

--- nn/models.py
+++ nn/models.py
@@ -1,1 +1,20 @@
 
+
+class Sequential:
+
+    def __init__(self, *layers):
+        self.layers = [*layers]
+
+    def forward(self, batch):
+        activation = batch
+        for layer in self.layers:
+            activation = layer.forward(activation)
+
+        return activation
+
+    def backward(self, batch, targets):
+
+        output = self.forward(batch)
+        error = self.layers[-1].backward(output, targets)
+        for layer in reversed(self.layers[:-1]):
+            error = layer.backward(error)
