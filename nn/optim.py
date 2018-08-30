81575180ad99344f8bd946c0ab68e2d9a0c7024e
DylanV <dverrezen@gmail.com>
2018-08-30 21:22:40 +0200

    Add adagrad

--- nn/optim.py
+++ nn/optim.py
@@ -130,3 +130,43 @@ class NesterovSGD:
             sleep(0.01)  # Tiny sleep so the progress bar updates correctly
             print(f'Epoch {self.num_epochs:2d}: {current_loss:0.2e}')
 
+
+class Adagrad:
+
+    def __init__(self, parameters, grad_func):
+        self.parameters = parameters
+        self.grad_func = grad_func
+        self.grad_cache = [np.zeros(param.shape) for param in self.parameters]
+
+        self.num_epochs = 0
+
+    def train(self, data, targets, epochs, batch_size, learning_rate, regularisation_weight,
+              regulariser='L2'):
+        num_samples = data.shape[0]
+        batch_starts = np.arange(batch_size, num_samples, batch_size)
+        idxes = np.arange(0, num_samples, 1)
+
+        for epoch in range(epochs):
+            self.num_epochs += 1
+            # Shuffle the ids and split into mini-batches
+            np.random.shuffle(idxes)
+            batches = np.split(idxes, batch_starts)
+            current_loss = 0
+            for batch in tqdm(batches, leave=False, desc=f'Epoch {self.num_epochs:2d}: '):
+                loss, gradients = self.grad_func(data[batch], targets[batch])
+                # Exponential moving average of the loss
+                current_loss = 0.01 * loss + (1 - 0.99) * current_loss if loss != 0 else loss
+                # Update the parameters
+                delta = learning_rate / batch_size
+                for param, grad, cache in zip(self.parameters, gradients, self.grad_cache):
+                    # Do any regularization
+                    if regulariser == 'L1':
+                        param -= delta * regularisation_weight * np.sign(param)
+                    elif regulariser == 'L2':
+                        param -= regularisation_weight * delta * param
+                    cache += grad ** 2
+                    # Step down the gradient
+                    param -= delta * (grad / (np.sqrt(cache) + 1e-8))
+            sleep(0.01)  # Tiny sleep so the progress bar updates correctly
+            print(f'Epoch {self.num_epochs:2d}: {current_loss:0.2e}')
+
